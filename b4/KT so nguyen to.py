93165bb415f9446e50826d6016fa2f975be36838
Tung-1991 <63487060+Tung-1991@users.noreply.github.com>
2021-02-02 12:49:47 +0700

    b5

--- b4/KT so nguyen to.py
+++ b4/KT so nguyen to.py
@@ -1,21 +1,15 @@
-n = int(input("nhap n: "))
+n = int(input('nhap n: '))
 snt = True
-#kiem tra snt
-if n < 2:
-    snt = False
+if n <2:
+    snt= False
 else:
     i=2
-    #vong lap voi i la uoc cua n
-    #chia 2 de kiem tra so le
-    while(i <= n/2):
-        if (n % i ==0):
+    while(i<n-1):
+        if(n%i==0):
             snt=False
-            break;
+            break
         i = i+1
-#neu snt = true
 if snt == True:
-    print(n ,'la so nguyen to')
+    print(n,"la snt")
 else:
-    print(n,'khong la so nguyen to')
-
-print(n/2)
+    print(n,"khong la snt")
---
+++ b5/sap xep value.py
@@ -0,0 +1,3 @@
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+y = dict(sorted(x.items()))
+print(y)
